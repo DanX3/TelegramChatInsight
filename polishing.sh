e2a9b967b09096a837f5b95f64166d9319d99650
Daniele Tolomelli <daniele.tolomelli@akka.eu>
2019-01-31 14:38:55 +0100

    Fixed some blacklist and adapted for general purpose names blacklist

--- polishing.sh
+++ polishing.sh
@@ -1,12 +1,20 @@
 cat ChatExport*/messages* \
     | sed -E '/<\/?(div|a|body|html|head|script|meta|link|title)/d' \
-    | sed -E '/(D|Daniele |S<|Stalky &lt;3|Previous messages|Next messages|<!DOCTYPE html>)$/d' \
     | sed '/^$/d' \
+    | sed -E '/^[A-Z][A-Z]?\ ?$/d' \
+    | sed -E '/^[A-Z][a-z]+\ [A-Z][a-z]+\ ?$/d' \
+    | sed -E '/^[A-Z][a-z]+\ ?$/d' \
+    | sed -E '/^(S<|Stalky &lt;3)\ ?$/d' \
+    | sed -E '/[0-9]\ (K|M)B$/d' \
+    | sed -E '/(Foto|Video)\ da/d' \
+    | sed -E '/\.mp4$/d' \
+    | sed -E '/^(Previous message|Next message|<!DOCTYPE html>)$/d' \
+    | sed '/^Not\ included/d' \
     | sed '/[0-9][0-9]:[0-9][0-9]/d' \
-    | sed '/via @gif/,+3 d' \
-    | sed -E '/^(Sticker|Photo|Animation)$/,+2 d' \
+    | sed '/via @gif/,+1 d' \
+    | sed -E '/^(Photo|Animation)$/,+2 d' \
     | sed -E '/^(Location|Voice message|Video message|Video file)$/,+1 d' \
     | sed -E '/[0-9]+\ [A-Z][a-z]+\ [0-9]{4}/d' \
     | sed "s/&apos;/'/g" \
     | sed 's/&quot;/"/g' \
-    > polished
+    > cleaned
